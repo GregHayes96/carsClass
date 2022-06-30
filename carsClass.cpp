473b8ab6289e616542751556c5b653cf82d7055b
GregHayes96 <102591788+GregHayes96@users.noreply.github.com>
2022-06-30 16:40:42 +0100

    Feature branch testing for second version of code

--- carsClass.cpp
+++ carsClass.cpp
@@ -3,18 +3,23 @@
 class Cars {
 public:
     //default constructor
-    Cars(std::string colour = "N/A", std::string make = "N/A", int miles = 0) : colour(colour), make(make), miles_on_clock(miles) {} ;
+    Cars(std::string colour = "N/A", std::string make = "N/A", int miles = 0) : colour(colour), make(make), miles_on_clock(miles), service_due(false), mot_due(false) {};
     //getter
     int get_miles()const { return miles_on_clock; }
     std::string get_colour() const { return colour; }
     std::string get_make() const { return make; }
-    
+    //setter
+    void add_miles(int miles);
+    //print
     void print();
- 
+    //member functions
+    bool service_check(int miles);
 private:
     std::string colour;
     std::string make;
     int miles_on_clock;
+    bool service_due;
+    bool mot_due;
 };
 
 //-----------------------------------------------------------------------------
@@ -27,11 +32,32 @@ void Cars::print() {
     std::cout << std::endl;
 }
 
+void Cars::add_miles(int miles) {
+    service_check(miles);
+    this->miles_on_clock += miles;
+}
+
+//every 10,000 miles service is due, if the miles added pushes over 10,000 miles service is changed to true.
+bool Cars::service_check(int miles) {
+    int temp{ miles_on_clock };
+    while (temp > 10000) {
+        temp -= 10000;
+    }
+    temp = 10000 - temp;
+    if (miles >= temp) {
+        service_due = true;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 int main() {
     Cars Ford("white", "Ford focus", 1000);
     Ford.print();
     Cars BMW("Blue", "1 series");
     BMW.print();
-    std::cout << "version 1";
+    std::cout << "version 2";
 
 }
